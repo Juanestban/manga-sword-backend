2dde14c6c303a9423d04352483ff0ebf5fbc8edc
Juan esteban <juanestban000@gmail.com>
2021-07-27 23:31:57 -0500

    Feat: add new rule for lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,17 @@
 module.exports = {
   env: {
     es2021: true,
-    node: true
+    node: true,
   },
-  extends: [
-    'standard'
-  ],
+  extends: ['standard'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 12,
-    sourceType: 'module'
+    sourceType: 'module',
   },
-  plugins: [
-    '@typescript-eslint'
-  ],
+  plugins: ['@typescript-eslint'],
   rules: {
-  }
+    'comma-dangle': 0,
+    'import/first': 0,
+  },
 }
