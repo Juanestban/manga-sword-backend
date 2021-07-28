ac18c440a115765e44de67c79b2586e9283c9620
Juan esteban <juanestban000@gmail.com>
2021-07-27 23:32:13 -0500

    Feat: Add the connection to database in mongodb

---
+++ src/database.ts
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose'
+import { Keys, Credentials } from './models/Database'
+
+const cred: Credentials = {
+  user: 'mongoadmin',
+  password: 'mongoadmin',
+}
+
+const keys: Keys = {
+  URI: `mongodb://${cred.user}:${cred.password}@localhost:27017/MangaSword?authSource=admin&retryWrites=true`,
+}
+
+mongoose
+  .connect(keys.URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    auth: {
+      user: cred.user,
+      password: cred.password,
+    },
+  })
+  .then(() => console.log('mongoDB is conected'))
+  .catch((err) => console.error(err))
