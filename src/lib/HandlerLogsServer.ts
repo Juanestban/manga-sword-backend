0e83ee24cc494047a9c35100c99a19b16ae9ca87
Juan esteban <juanestban000@gmail.com>
2021-08-01 21:02:35 -0500

    Refactor: change functions console

--- src/lib/HandlerLogsServer.ts
+++ src/lib/HandlerLogsServer.ts
@@ -1,7 +1,13 @@
 export class HandlerLogsServer {
-  public wherever: string = ''
-
-  HandlerLogsServer() {
+  HandlerLogsServer(): void {
     console.log('handler logs server')
+  }
+
+  getNormalMessage(message: string): void {
+    console.log(`[+] ${message}`)
+  }
+
+  getErrorMessage(msgError: string): void {
+    console.error(`[-] ${msgError}`)
   }
 }
